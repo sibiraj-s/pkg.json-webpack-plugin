de76a7267f8e65772bb9fd1bceb173d7bba93a46
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2025-11-18 11:39:59 +0530

    build: replace tsup with tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   entry: ['src/index.ts'],
   outDir: 'lib',
-  splitting: false,
   sourcemap: true,
   clean: true,
   format: ['cjs'],
